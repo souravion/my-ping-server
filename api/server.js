29cb4c3fba463bac678eec5b7b809cbb72591e2a
sourav@ion <sourav.ion@gmail.com>
2025-05-18 13:29:23 +0530

    add vercel config

--- api/server.js
+++ api/server.js
@@ -1,11 +1,16 @@
+// server.js
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors()); // Allow all origins (adjust for security in prod)
+
 app.get('/api/ping', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-  });
+  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.set('Pragma', 'no-cache');
+  res.set('Expires', '0');
+  res.status(200).json({ status: 'success' });
 });
 
 app.listen(PORT, () => {
